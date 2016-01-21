3da405b68fedac27b8c7638ca1496a977d1b6115
prpella <ela@posteo.de>
2016-01-21 16:46:48 +0100

    add usage hint -h

--- pk.sh
+++ pk.sh
@@ -1,5 +1,9 @@
 if [[ $# = 0 ]]; then
     liste="$GITHUB_NAME"
+elif [[ $1 = "-h" ]]; then
+    echo "usage: pk.sh [GithubName]"
+    echo "add \$GITHUB_NAME=[yourGithubName] to .bashrc to use pk.sh without argument" 
+    exit 0
 else
     liste="$@"
 fi
