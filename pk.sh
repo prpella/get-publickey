533d52f10283d5eb104eae2efe2ae39d0f51f290
jumee <judith.mewes@actano.de>
2016-01-28 10:57:53 +0100

    add function of "-help"

--- pk.sh
+++ pk.sh
@@ -1,6 +1,6 @@
 if [[ $# = 0 ]]; then
     liste="$GITHUBNAME"
-elif [[ $1 = "-h" ]]; then
+elif [[ $1 = "-h" || $1 = "-help" ]]; then
     echo "usage: pk.sh [GithubName]"
     echo "add \$GITHUBNAME=[yourGithubName] to .bashrc to use pk.sh without argument" 
     exit 0
